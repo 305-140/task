8ddc58971d321bf14ab8789b7c243f7588493e5f
Andrey <semenovandrey19861986@mail.ru>
2023-02-15 22:24:19 +0400

    Added Task03, Task05

---
+++ Task03/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите номер дня недели:");
+int numberWeek = Convert.ToInt32(Console.ReadLine());
+if (numberWeek == 1) 
+{
+    Console.WriteLine("Понедельник");
+}
+else if (numberWeek == 2) 
+{
+    Console.WriteLine("Вторник");
+}
+else if (numberWeek == 3) 
+{
+   Console.WriteLine("Среда");
+}
+else if (numberWeek == 4) 
+{
+    Console.WriteLine("Четверг");
+}
+else if (numberWeek == 5) 
+{
+   Console.WriteLine("Пятница");
+}
+else if (numberWeek == 6) 
+{
+    Console.WriteLine("Суббота");
+}
+else if (numberWeek == 7) 
+{
+    Console.WriteLine("Воскресенье");
+}
+else  
+{
+    Console.WriteLine("Такого дня не существует!");
+}

850313fba4e66b08fd4297937e674d2ff14f801d
Andrey <semenovandrey19861986@mail.ru>
2023-02-18 00:23:13 +0400

    практическое домашнее задание 3

---
+++ Task19/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 9999 && number < 100000)
+{
+    if (number == reNumber(number))   Console.WriteLine(reNumber(number)+" -> да");
+    else Console.WriteLine(number+" -> нет");
+}
+else Console.WriteLine("Это не пятизначное число ");
+
+//Console.WriteLine(reNumber(number));
+
+int reNumber(int num)
+{
+    int reNum = 0;
+    while (num > 0)
+    {
+        reNum = reNum * 10 + num % 10;
+        num = num / 10;
+    }
+    return reNum;
+}
---
+++ Task21/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координаты точки A(х,y,z):");
+int numX1 = Convert.ToInt32(Console.ReadLine());
+int numY1 = Convert.ToInt32(Console.ReadLine());
+int numZ1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B(х,y,z):");
+int numX2 = Convert.ToInt32(Console.ReadLine());
+int numY2 = Convert.ToInt32(Console.ReadLine());
+int numZ2 = Convert.ToInt32(Console.ReadLine());
+
+// double d = 5.09998774;
+// double dRound = Math.Round(d, 2, MidpointRounding.ToZero);
+Console.WriteLine(Math.Round(Distance(numX1, numY1, numZ1, numX2, numY2, numZ2), 2, MidpointRounding.ToZero));
+
+double Distance(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double dis = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+    return dis;
+}

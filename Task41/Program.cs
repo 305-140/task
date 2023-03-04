79197fadb3d6bb6127bffa0b25a61ab9052e4254
Andrey <semenovandrey19861986@mail.ru>
2023-03-04 15:29:27 +0400

    41,43

---
+++ Task41/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите M: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array;
+array = ArrayM(number);
+
+PrintArray(array);
+Console.WriteLine($" -> {Counts(array)}");
+
+
+int Counts(int[] arr)
+{
+    int num = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+       if (arr[i]>0)  num++;
+    }
+    return num;
+}
+
+int[] ArrayM(int num)
+{
+    int[] arr = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+
---
+++ Task43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("y = k1 * x + b1, y = k2 * x + b2");
+Console.WriteLine("Введите  k1,b1,k2,b2:");
+int numk1 = Convert.ToInt32(Console.ReadLine());
+int numb1 = Convert.ToInt32(Console.ReadLine());
+int numk2 = Convert.ToInt32(Console.ReadLine());
+int numb2 = Convert.ToInt32(Console.ReadLine());
+
+double[] array = CommonPoint(numk1, numb1, numk2, numb2);
+Console.WriteLine("Точка пересечения: " + array[0]+";"+array[1]);
+
+double[] CommonPoint(int k1, int b1, int k2, int b2)
+{
+    double[] mass = new double[2] {0,0};
+    if (k1 == k2) return mass;
+    else
+    {
+        mass[0] = ((b2 - b1)/(k1 - k2));
+        // x = Math.Round(x, 2, MidpointRounding.ToZero);
+        mass[1] = k1 * mass[0] + b1;
+        // y = Math.Round(y, 2, MidpointRounding.ToZero);
+        return mass;
+    }
+}

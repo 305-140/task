236c3b80b92006bfe2945be1f06eae421454dfa3
Andrey <semenovandrey19861986@mail.ru>
2023-03-04 12:07:38 +0400

    добавлены задания

---
+++ Task42/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите десятичное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{number} -> " + Convert.ToString(number, 2));
+Console.WriteLine($"{number} -> {DecToBin(number)}");
+
+string DecToBin(int num)
+{
+    string temp = "";
+    while (num > 1)
+    {
+        temp = num % 2 + temp;
+        num = num / 2;
+    }
+    temp = num + temp;
+    return temp;
+}
+
---
+++ Task44/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+int[] CreateArrayIntFib(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
